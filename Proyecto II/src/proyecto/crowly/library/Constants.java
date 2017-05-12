1bdbe2a68abb4fca1af9e25dcd5ee6f9f97d4412
dnnxl15 <dnnxl15@hotmail.com>
2017-05-11 18:17:04 -0600

    Update Constants.java

--- Proyecto II/src/proyecto/crowly/library/Constants.java
+++ Proyecto II/src/proyecto/crowly/library/Constants.java
@@ -2,5 +2,25 @@ package proyecto.crowly.library;
 
 public class Constants 
 {
-
+  public final String GOOGLE_CHROME = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe "; // Aplication direction.
+		
+	public final String TIME_HEADER = "Time";
+	
+	public final String PEOPLE_HEADER = "People";
+	
+	public final String IDENTIFIER = "@@DATA@@";
+	
+	public final String[] LIST_OF_FILES_CHARTS = 
+	{
+			"bar.html",
+			"scatter.html",
+			"line.html"
+	};
+	
+	public final String[] LIST_OF_HEADERS_CHARTS = 
+	{
+			"C:/Users/dnnxl/OneDrive/Documentos/Proyecto Crowly/Documents/BarChartHeader.txt",
+			"C:/Users/dnnxl/OneDrive/Documentos/Proyecto Crowly/Documents/LineChartHeader.txt",
+			"C:/Users/dnnxl/OneDrive/Documentos/Proyecto Crowly/Documents/ScatterChartHeader.txt"
+	};
 }
